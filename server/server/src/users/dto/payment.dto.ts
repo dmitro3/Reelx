230c34381c080ad8d20d4f801569244da9887663
Александр Носиков <aleksandrnosikov@MacBook-Pro-Aleksandr.local>
2026-01-20 17:15:45 +0400

    fix dto

--- server/server/src/users/dto/payment.dto.ts
+++ server/server/src/users/dto/payment.dto.ts
@@ -1,4 +1,12 @@
+import { IsIn, IsNumber, IsPositive } from 'class-validator';
+import { Type } from 'class-transformer';
+
 export class PaymentDto {
+    @Type(() => Number)
+    @IsNumber()
+    @IsPositive()
     amount: number;
+
+    @IsIn(['stars'])
     type: 'stars';
 }
