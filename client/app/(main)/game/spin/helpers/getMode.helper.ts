341733ee6c96afc40e381968ac95174b8ac71f81
Александр Носиков <aleksandrnosikov@MacBook-Pro-Aleksandr.local>
2026-01-28 15:44:32 +0400

    add secret

--- client/app/(main)/game/spin/helpers/getMode.helper.ts
+++ client/app/(main)/game/spin/helpers/getMode.helper.ts
@@ -3,11 +3,11 @@ import { CurrencyType } from "../hooks/useCurrency";
 export function getMode(currency: CurrencyType, totalPrice: number): 'normal' | 'mystery' | 'multy' {
     if(currency === 'stars' && totalPrice > 250) {
         return 'mystery';
-    } else if(currency === 'ton' && totalPrice > 10) {
+    } else if(currency === 'ton' && totalPrice > 50) {
         return 'mystery';
     } else if(currency === 'stars' && totalPrice > 50) {
         return 'multy';
-    } else if(currency === 'ton' && totalPrice > 50) {
+    } else if(currency === 'ton' && totalPrice > 10) {
         return 'multy';
     }
     return 'normal';
